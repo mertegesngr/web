033fe47781726d9ec3a18ebac726527739cde92b
Mert Ege Sungur <mertegesungur5@gmail.com>
2025-06-03 12:44:50 +0300

    web sitem

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿const toggleButton = document.getElementById("theme-toggle");
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Tercihi localStorage'a kaydet
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Sayfa açıldığında temayı geri yükle
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+});
